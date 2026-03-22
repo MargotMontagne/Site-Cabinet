67f3aba8d42f6b778d416d96f2f0f9737020e380
Margot Montagne <margot.montagne.avocat@gmail.com>
2026-03-22 22:22:33 +0100

    fix: add unsafe-eval to CSP for GTM compatibility

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ export function middleware(request: NextRequest) {
 
   const csp = [
     "default-src 'self'",
-    `script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https://www.googletagmanager.com https://tagmanager.google.com https://www.google-analytics.com`,
+    `script-src 'self' 'nonce-${nonce}' 'strict-dynamic' 'unsafe-eval' https://www.googletagmanager.com https://tagmanager.google.com https://www.google-analytics.com`,
     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
     "font-src 'self' https://fonts.gstatic.com",
     "img-src 'self' data: blob: https://www.googletagmanager.com https://www.google-analytics.com https://stats.g.doubleclick.net",
