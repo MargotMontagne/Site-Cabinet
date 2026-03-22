c8d14c62d7d3cb9a19bd35c03fa056c8a2f3d133
Margot Montagne <margot.montagne.avocat@gmail.com>
2026-03-22 21:07:02 +0100

    fix: allow googletagmanager.com in CSP script-src

--- middleware.ts
+++ middleware.ts
@@ -6,12 +6,12 @@ export function middleware(request: NextRequest) {
 
   const csp = [
     "default-src 'self'",
-    `script-src 'self' 'nonce-${nonce}' 'strict-dynamic'`,
+    `script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https://www.googletagmanager.com`,
     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
     "font-src 'self' https://fonts.gstatic.com",
-    "img-src 'self' data: blob:",
-    "frame-src https://cal.eu https://maps.google.com https://www.google.com https://maps.googleapis.com",
-    "connect-src 'self' https://formspree.io",
+    "img-src 'self' data: blob: https://www.googletagmanager.com https://www.google-analytics.com https://stats.g.doubleclick.net",
+    "frame-src https://cal.eu https://maps.google.com https://www.google.com https://maps.googleapis.com https://www.googletagmanager.com",
+    "connect-src 'self' https://formspree.io https://www.google-analytics.com https://analytics.google.com https://stats.g.doubleclick.net https://region1.analytics.google.com",
     "object-src 'none'",
     "base-uri 'self'",
     "form-action 'self'",
